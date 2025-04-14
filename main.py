782c9d7131fea45656601c90b06519909065e948
ThomasVinter <tv@javitech.dk>
2025-04-14 18:41:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ def get_tider():
     result = beregn_timer(data, CHEAPEST_HOURS, INVERTED)
     return result
     if __name__ == "__main__":
-    import uvicorn
-    import os
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+        import uvicorn
+        import os
+        port = int(os.environ.get("PORT", 8000))
+        uvicorn.run("main:app", host="0.0.0.0", port=port)
 
